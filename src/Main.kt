c86591b18c597eedaff9dd82ff23fc2271e5f9f1
M1-49-mariam <mariam.almutairi19@gmail.com>
2025-02-17 18:51:48 +0300

    added Kelvin

--- src/Main.kt
+++ src/Main.kt
@@ -1,6 +1,8 @@
 fun main() {
 
-    println("Temperature in Fahrenheir: ${TemperatureConverter.convertCelsiusToFahrenheit(30.0)}")
+    println("Temperature in Fahrenheit: ${TemperatureConverter.convertCelsiusToFahrenheit(30.0)}")
     println("Temperature in Celsius: ${TemperatureConverter.convertFahrenheitToCelsius(100.0)}")
+    println("Temperature from Fahrenheit to Kelvin: ${TemperatureConverter.convertFahrenheitToKelvin(100.0)}")
+    println("Temperature from Celsius to Kelvin: ${TemperatureConverter.convertCelsiusToKelvin(40.0)}")
 
 }
--- src/TemperatureConverter.kt
+++ src/TemperatureConverter.kt
@@ -1,19 +1,19 @@
 class TemperatureConverter {
     companion object {
-        var temperature: Double = 0.0
+
         fun convertCelsiusToFahrenheit(temperature: Double): Double {
             return temperature * 1.8 + 32
         }
+
         fun convertFahrenheitToCelsius(temperature: Double): Double {
             return (temperature - 32) / 1.8
         }
+
+        fun convertFahrenheitToKelvin(temperature: Double): Double {
+            return (temperature - 32) / 1.8 + 273.15
+        }
+        fun convertCelsiusToKelvin(temperature: Double): Double {
+            return temperature + 273.15
+        }
     }
-
-   //3. **Using the Companion Object**
-    //    - Create a main function or similar entry point.
-    //    - Use the methods from the companion object to convert temperatures.
-    //4. **Enhancing the Companion Object**
-    //    - Add more utility methods as needed (e.g., Kelvin conversions).
-    //5. **Testing the Conversions**
-
 }
